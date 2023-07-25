4d68d2fd25aa8c9d6565e1c125b359a9e50b3734
rdheadyii <rdheadyii@gmail.com>
2023-07-25 19:31:19 -0400

    added starter classes and imports/exports

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // need variables to import inquirer, fs, and shapes.js
-
+import inquirer from "inquirer";
+import fs from "fs";
+import {Circle, Square, Triangle} from "module";
 // need variable for question array 
-
+const questions = [
+    
+]
 // need function to prompt on init
 
 // need function to generate svg
