56fd393b8f3a39462db0e57032405d32686135de
rdheadyii <rdheadyii@gmail.com>
2023-07-25 20:22:36 -0400

    fixed import for shapes and wrote shapes classes

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // need variables to import inquirer, fs, and shapes.js
 import inquirer from "inquirer";
 import fs from "fs";
-import {Circle, Square, Triangle} from "module";
+import {Circle, Square, Triangle} from "./lib/shapes.js";
 // need variable for question array 
 const questions = [
-    
+
 ]
 // need function to prompt on init
 
